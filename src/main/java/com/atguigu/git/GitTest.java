eeebcc506549d1488d02478903acf2a579355e8f
Cynicism <1195497448@qq.com>
2022-02-13 14:49:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author cynicism
+ * @create 2022-02-13 14:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
